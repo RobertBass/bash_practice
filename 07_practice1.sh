c13f48f9346d0b43169a83470ad07f6da6f25e6d
Roberto Bravo <robertbass@hotmail.com>
2024-12-09 10:19:03 -0500

    INTERACTIVIDAD CON USUARIO

---
+++ 07_practice1.sh
@@ -0,0 +1,10 @@
+# !/bin/bash
+# Programa interactivo
+# Autor: Roberto Bravo
+
+echo "Cual es tu nombre? "
+read nombre
+echo "Cual es tu edad? "
+read edad
+
+echo "Listo, tu nombre es $nombre y tienes $edad anios"
