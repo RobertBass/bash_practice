23b232c46eea543069d1fdec900ac7ea015c9b9e
Roberto Bravo <robertbass@hotmail.com>
2024-12-09 10:10:28 -0500

    INITIAL COMMIT WITH 6 EXAMPLES OF USING BASH

--- 05_argumentos.sh
+++ 05_argumentos.sh
@@ -7,3 +7,6 @@ horarioCurso=$2
 echo "El nombre del curso es $nombreCurso dictado en el horario $horarioCurso"
 echo "El numero de parametros enviados es $#"
 echo "Los parametros enviados son: $*"
+
+# PARA EJECUTAR
+# ./05_argumentos.sh "parametro1" "parametro2"
